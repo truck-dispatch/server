5cc1b8f594ea43b637b2327bb4804e1d1b24a3f7
god-of-js <henryeze019@gmail.com>
2023-04-02 18:29:08 +0100

    feat(): implemented add and update account

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,5 +1,4 @@
 import { userTypes } from '../common/constants'
-import Asset from './Asset'
 import TransferRecipient from './TransferRecipient'
 
 export default interface User {
@@ -8,7 +7,7 @@ export default interface User {
   lastName: string
   email: string
   phone: string
-  avatar?: Asset | File
+  avatar?: string | File
   userType: (typeof userTypes)[number]
   status?:
     | 'pending_verification'
