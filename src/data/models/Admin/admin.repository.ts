990e02551b05f10f24228d8a522141ab47a072c0
chukwuekeuzoma <chukwuekeuzoma@gmail.com>
2023-04-07 13:03:37 +0100

    chore:created finAdminBy funtion that's query in the database

--- src/data/models/Admin/admin.repository.ts
+++ src/data/models/Admin/admin.repository.ts
@@ -5,3 +5,18 @@ export function createAdmin(admin: Partial<Admin>) {
   const data = new AdminModel(admin)
   return data.save()
 }
+
+export async function findAdminBy(
+  param: Partial<Admin>,
+  deletePassword = true
+): Promise<Admin | null> {
+  const user = await AdminModel.findOne(param)
+  if (!user) {
+    return null
+  }
+
+  if (!deletePassword) return user.toObject()
+
+  const { password, ...userWithoutPassword } = user.toObject()
+  return userWithoutPassword as unknown as Admin
+}
