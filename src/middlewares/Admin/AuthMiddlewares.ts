730372af8aa63c36ace67e6111cc326bca5e9498
chukwuekeuzoma <chukwuekeuzoma@gmail.com>
2023-04-07 13:04:31 +0100

    chore:created registrationChecks method middleware for adim

---
+++ src/middlewares/Admin/AuthMiddlewares.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from 'express'
+import { Helpers } from '../../helpers'
+import Respond from '../../helpers/Respond'
+import { findAdminBy } from '../../data/models/Admin/admin.repository'
+
+class AuthMiddlewares {
+  async registrationCredentialsChecks(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const { email, firstName, lastName, password } = req.body
+      const isValidEmail = Helpers.isValidEmail(email)
+      if (!isValidEmail) {
+        return Respond.error(res, 'Provide a valid email address', 400)
+      }
+      if (!email || !firstName || !lastName || !password) {
+        return Respond.error(
+          res,
+          'firstName, lastName, email, password  are compulsory fields.',
+          400
+        )
+      }
+      const adminWithEmailExists = await findAdminBy({ email })
+      if (!!adminWithEmailExists) {
+        return Respond.error(
+          res,
+          'This email already exists on our database.',
+          400
+        )
+      }
+      next()
+    } catch (err) {
+      // Report error to our client..
+      console.log(err)
+      Respond.error(res, 'Something went wrong...')
+    }
+  }
+}
