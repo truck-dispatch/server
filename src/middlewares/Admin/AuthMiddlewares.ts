2993e88a4a67a2aa7b54d849985377e0f32d99bb
chukwuekeuzoma <chukwuekeuzoma@gmail.com>
2023-04-10 23:04:23 +0100

    chore:added login checks for admin

--- src/middlewares/Admin/AuthMiddlewares.ts
+++ src/middlewares/Admin/AuthMiddlewares.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express'
 import { Helpers } from '../../helpers'
 import Respond from '../../helpers/Respond'
 import { findAdminBy } from '../../data/models/Admin/admin.repository'
+import { compareHashAndPassword } from '../../services/encrypt'
 
 class AuthMiddlewares {
   async registrationCredentialsChecks(
@@ -35,6 +36,37 @@ class AuthMiddlewares {
       // Report error to our client..
       console.log(err)
       Respond.error(res, 'Something went wrong...')
+    }
+  }
+
+  async loginCredentialChecks(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { email, password } = req.body
+      const isValidEmail = Helpers.isValidEmail(email)
+
+      if (!isValidEmail)
+        return Respond.error(res, 'Provide a valid email address')
+
+      const admin = await findAdminBy({ email }, false)
+      if (!admin) {
+        return Respond.error(
+          res,
+          'Admin with this email does not exist in our database',
+          400
+        )
+      }
+      const passwordsMatch = await compareHashAndPassword(
+        admin.password,
+        password
+      )
+      if (!passwordsMatch) {
+        return Respond.error(res, 'Email and Password do not match', 400)
+      }
+
+      next()
+    } catch (err) {
+      console.log(err)
+      return Respond.error(res, `${err}`)
     }
   }
 }
