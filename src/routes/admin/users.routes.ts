662f2008cb9814a49400fa348631c3aee8023309
god-of-js <henryeze019@gmail.com>
2023-04-19 13:21:35 +0100

    chore(): format

--- src/routes/admin/users.routes.ts
+++ src/routes/admin/users.routes.ts
@@ -1,7 +1,7 @@
-import { Router } from 'express';
+import { Router } from 'express'
 
-import UserController from '@controllers/admin/UserController';
+import UserController from '@controllers/admin/UserController'
 
-const router = Router();
+const router = Router()
 
 router.get('/users', UserController.getUsers)
