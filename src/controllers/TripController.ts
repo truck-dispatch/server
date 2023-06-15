b421049c1d9c0bfac6ee008e3f1555d6999f4923
god-of-js <henryeze019@gmail.com>
2023-06-15 14:39:52 +0100

    chore(): email urls synchronized with the frontend

--- src/controllers/TripController.ts
+++ src/controllers/TripController.ts
@@ -359,7 +359,7 @@ async function setTripToCompleted(tripId: string) {
   const tripOwner = await findUserBy({ _id: updatedTrip?.tripOwner._id })
   Mail.tripHasBeenSetToCompleted(
     tripOwner?.email!,
-    `${FRONTEND_URL}/my-trips/${tripId}/status`,
+    `${FRONTEND_URL}/my-trips/${tripId}`,
     `${user?.firstName} ${user?.lastName}`
   )
 
@@ -375,7 +375,7 @@ async function setTripToInProgress(tripId: string) {
   const tripOwner = await findUserBy({ _id: updatedTrip?.tripOwner._id })
   Mail.tripHasBeenSetToInProgress(
     tripOwner?.email!,
-    `${FRONTEND_URL}/my-trips/${tripId}/status`,
+    `${FRONTEND_URL}/my-trips/${tripId}`,
     `${user?.firstName} ${user?.lastName}`
   )
 
