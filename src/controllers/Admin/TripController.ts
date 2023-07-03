e58dafd52156731455e56d63823a164036802864
chukwuekeuzoma <chukwuekeuzoma@gmail.com>
2023-07-03 10:28:46 +0100

    refactore:added pagination

--- src/controllers/Admin/TripController.ts
+++ src/controllers/Admin/TripController.ts
@@ -1,12 +1,18 @@
 import { NextFunction, Request, Response } from 'express'
 import { findTripsBy } from '@data/trip/tripRepository'
 import Respond from '@helpers/Respond'
+import Trip from '@interfaces/Trip'
+import { tripStatus } from '@common/constants'
 
 class TripController {
   async getTrips(req: Request, res: Response, next: NextFunction) {
+    const { page, limit, status } = req.query
     try {
-      const trips = await findTripsBy({})
-      return Respond.success(res, 'Trips fetched successfully', trips)
+      const paramToFetchWith: Partial<Trip> = {}
+      if (tripStatus.includes(status as string))
+        paramToFetchWith.status = status as string
+      const trips = await findTripsBy(paramToFetchWith, page as string, limit as string)
+      return Respond.success(res, 'Trips fetched successfully', {...trips})
     } catch (err) {
       next(err)
     }
